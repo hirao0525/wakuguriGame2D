1a618087bc6ae41977e0a1049e74be6f889425b1
0n0cats <straycats.30s.yumedeath@gmail.com>
2013-06-12 14:38:16 +0900

    円がランダムで発生することと飛行機のタッチ時のイベントを修正したよ

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ rand = function(num) {
 }
 
 // add drow circle method to surface
-Surface.prototype.drawCircle = function(x, y, r) {
+Surface.prototype.drawCircle = function(x, y, r, color) {
         this.context.beginPath();
-        this.context.strokeStyle = "blue";  // test
+        this.context.strokeStyle = color;
         this.context.arc(x, y, r, 0, Math.PI * 2, true);
         this.context.stroke();
 };
@@ -26,12 +26,6 @@ Plane = Class.create(Sprite, {
 		this.y = y;
 		this.frame = 0;
 		// define touch event
-		this.addEventListener('touchstart', 
-			function(e) { this.x = e.x - 16; this.y = e.y - 16; });
-		this.addEventListener('touchend',
-			function(e) { this.x = e.x - 16; this.y = e.y - 16; });
-		this.addEventListener('touchmove', 
-			function(e) { this.x = e.x - 16; this.y = e.y- 16; });
 	}
 });
 
@@ -53,29 +47,13 @@ ScoreLabel = Class.create(Label, {
 	}
 });
 
-// DrowRing class define
-// inheritance Sprite class
-DrowRing = Class.create(Sprite, {
-        initialize:function(x, y) {
-                // call Sprite class constractor
-                enchant.Sprite.call(this, 50, 50);
-                this.surface = new Surface(50, 50);
-                // ring color is red
-                this.surface.drawCircle(25, 25, 10);
-                this.image = this.surface;
-                this.x = x;
-                this.y = y;
-                this.frame = 0;
-        }
-});
-
 // initialize
 window.onload = function() {
 	game = new Game(WIDTH, HEIGHT);
 	// load image droid
-	game.preload('chara1.gif', 'background.jpg');
-    game.tick = 0;
-    game.fps = 16;
+	game.preload('chara1.gif', 'background.jpg');	
+ 	game.tick = 0;
+  	game.fps = 16;
 	game.onload = function() {
 		// create background
 		var bg = new Sprite(WIDTH, HEIGHT);
@@ -87,9 +65,42 @@ window.onload = function() {
 		// create plane
 		var plane = new Plane( WIDTH/2, HEIGHT/2 );
 		game.rootScene.addChild(plane);
-        // create ring
-        var ring = new DrowRing( 100, 100 );
-        game.rootScene.addChild(ring);
+		
+		game.addSurface = function(x, y, color) {
+			// create surface
+			var surface = new Surface(50, 50);
+			surface.drawCircle(25, 25, 10, color);
+
+			var sprite = new Sprite(50, 50);
+			sprite.image = surface;
+			sprite.x = x;
+			sprite.y = y;
+			game.rootScene.addChild(sprite);
+
+			// sprite processing
+			sprite.tick = 0;
+			sprite.addEventListener(Event.ENTER_FRAME, function() {
+				sprite.tick++;
+				sprite.scaleX += 0.05;
+				sprite.scaleY += 0.05;
+				if(sprite.tick > 80) game.rootScene.removeChild(sprite);
+			});
+		};
+
+		game.rootScene.addEventListener('touchstart', 
+			function(e) { plane.x = e.x - 16; plane.y = e.y - 16; });
+		game.rootScene.addEventListener('touchend',
+			function(e) { plane.x = e.x - 16; plane.y = e.y - 16; });
+		game.rootScene.addEventListener('touchmove', 
+			function(e) { plane.x = e.x - 16; plane.y = e.y- 16; });
+
+		game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
+			// create ring
+			game.tick++;
+			if( game.tick % 8 === 0) {
+                        game.addSurface(rand(220), rand(220), "red");			
+}
+		}); 
 	}
 	
 	game.start();
