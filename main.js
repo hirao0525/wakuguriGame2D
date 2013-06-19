df0034f2c3c8b47aa7ba31621cf6933d02f5ae94
0n0cats <straycats.30s.yumedeath@gmail.com>
2013-06-19 20:40:26 +0900

    Revert "輪のカラーバリエーションを増やして、スコアが増加するように変更したよ。"
    This reverts commit 44dcfc8dc6d908e504047ea03c8cf9789b91b978.

--- main.js
+++ main.js
@@ -8,11 +8,6 @@ rand = function(num) {
 	return Math.floor(Math.random() * num);
 }
 
-// calculate coordinate langth
-function getLength(x1, y1, x2, y2 ) {
-	return Math.sqrt(Math.pow(x2 - x1 , 2.0 ) + Math.pow(y2 - y1 , 2.0 )); 
-}
-
 // add drow circle method to surface
 Surface.prototype.drawCircle = function(x, y, r, color) {
         this.context.beginPath();
@@ -30,6 +25,7 @@ Plane = Class.create(Sprite, {
 		this.x = x;
 		this.y = y;
 		this.frame = 0;
+		// define touch event
 	}
 });
 
@@ -58,8 +54,6 @@ window.onload = function() {
 	game.preload('chara1.gif', 'background.jpg');	
  	game.tick = 0;
   	game.fps = 16;
-	createRings = 0;
-	deleteRings = 0;
 	game.onload = function() {
 		// create background
 		var bg = new Sprite(WIDTH, HEIGHT);
@@ -75,40 +69,21 @@ window.onload = function() {
 		game.addSurface = function(x, y, color) {
 			// create surface
 			var surface = new Surface(50, 50);
-			surface.drawCircle(25, 25, 5, color);
+			surface.drawCircle(25, 25, 10, color);
 
 			var sprite = new Sprite(50, 50);
-			var point = 50;
-			var scalePace = 0.08;
-			var addTick = 1;
 			sprite.image = surface;
 			sprite.x = x;
 			sprite.y = y;
 			game.rootScene.addChild(sprite);
 
-			if(color == "blue") {
-				point *= 3;
-			        scalePace *= 2;
-				addTick *= 2;
-			} else if(color == "green") {
-				point *= 5;
-				scalePace *= 3;
-				addTick *= 3;
-			}
-
 			// sprite processing
 			sprite.tick = 0;
 			sprite.addEventListener(Event.ENTER_FRAME, function() {
-				sprite.tick += addTick;
-				sprite.scaleX += scalePace;
-				sprite.scaleY += scalePace;
-				if(sprite.tick > 50) {
-					if(getLength(sprite.x, sprite.y, plane.x, plane.y) < 50) {
-						scoreLabel.add(point - Math.floor(getLength(sprite.x, sprite.y, plane.x, plane.y)));
-					}	
-					game.rootScene.removeChild(sprite);
-					deleteRings++;
-				}
+				sprite.tick++;
+				sprite.scaleX += 0.05;
+				sprite.scaleY += 0.05;
+				if(sprite.tick > 80) game.rootScene.removeChild(sprite);
 			});
 		};
 
@@ -122,15 +97,9 @@ window.onload = function() {
 		game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
 			// create ring
 			game.tick++;
-			if( game.tick % 32 === 0 && createRings < 30) {
-				createRings++;
-        	                game.addSurface(rand(220), rand(220), "red");
-				if( createRings % 5 == 0) {
-					game.addSurface(rand(200), rand(200), "blue");
-				} else if( createRings % 12 == 0) {
-					game.addSurface(rand(200), rand(200), "green");
-				}
-			}
+			if( game.tick % 8 === 0) {
+                        game.addSurface(rand(220), rand(220), "red");			
+}
 		}); 
 	}
 	
