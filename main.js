ef0b7eff028a34179be4b7625c0c0ef7d9812a09
0n0cats <straycats.30s.yumedeath@gmail.com>
2013-07-17 18:42:49 +0900

    インデント調節しました（それだけかよ）

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ enchant();
 const HEIGHT= 320;
 const WIDTH = 320;
 
-
 // create random number
 rand = function(num) {
 	return Math.floor(Math.random() * num);
@@ -15,10 +14,10 @@ function getLength(x1, y1, x2, y2 ) {
 
 // add drow circle method to surface
 Surface.prototype.drawCircle = function(x, y, r, color) {
-        this.context.beginPath();
-        this.context.strokeStyle = color;
-        this.context.arc(x, y, r, 0, Math.PI * 2, true);
-        this.context.stroke();
+	this.context.beginPath();
+	this.context.strokeStyle = color;
+	this.context.arc(x, y, r, 0, Math.PI * 2, true);
+	this.context.stroke();
 };
 
 // inheritance Sprite class
@@ -37,12 +36,13 @@ Plane = Class.create(Sprite, {
 // inheritance label class
 ScoreLabel = Class.create(Label, {
 	initialize:function(x, y) {
-	// call label class constractor
+		// call label class constractor
 		enchant.Label.call(this, "SCORE: 0");
 		this.x = x;
 		this.y = y;
 		this.score = 0;
 	},
+
 	// add score
 	add:function(pts) {
 		this.score += pts;
@@ -51,13 +51,13 @@ ScoreLabel = Class.create(Label, {
 	}
 });
 
-// initialize
+// game main 
 window.onload = function() {
 	game = new Game(WIDTH, HEIGHT);
 	// load image droid
 	game.preload('chara1.gif', 'background.jpg');	
- 	game.tick = 0;
-  	game.fps = 16;
+	game.tick = 0;
+	game.fps = 16;
 	createRings = 0;
 	deleteRings = 0;
 	game.onload = function() {
@@ -88,7 +88,7 @@ window.onload = function() {
 
 			if(color == "blue") {
 				point *= 3;
-			        scalePace *= 2;
+				scalePace *= 2;
 				addTick *= 2;
 			} else if(color == "green") {
 				point *= 5;
@@ -99,53 +99,53 @@ window.onload = function() {
 			// sprite processing
 			sprite.tick = 0;
 			sprite.addEventListener(Event.ENTER_FRAME, function() {
-				sprite.tick += addTick;
-				sprite.scaleX += scalePace;
-				sprite.scaleY += scalePace;
-				if(sprite.tick > 50) {
-					if(getLength(sprite.x, sprite.y, plane.x, plane.y) < 50) {
+					sprite.tick += addTick;
+					sprite.scaleX += scalePace;
+					sprite.scaleY += scalePace;
+					if(sprite.tick > 50) {
+						if(getLength(sprite.x, sprite.y, plane.x, plane.y) < 50) {
 						scoreLabel.add(point - Math.floor(getLength(sprite.x, sprite.y, plane.x, plane.y)));
-					}	
-					game.rootScene.removeChild(sprite);
-					deleteRings++;
-				}
+						}	
+						game.rootScene.removeChild(sprite);
+						deleteRings++;
+					}
 			});
 		};
 
 		game.rootScene.addEventListener('touchstart', 
-			function(e) {
-				if(getLength(e.x, e.y, plane.x, plane.y) < 35) {				   
-					plane.x = e.x - 16; 
-					plane.y = e.y - 16; 
-				}
-			});
+				function(e) {
+					if(getLength(e.x, e.y, plane.x, plane.y) < 35) {
+						plane.x = e.x - 16; 
+						plane.y = e.y - 16; 
+					}
+				});
 		game.rootScene.addEventListener('touchend',
-			function(e) {
-				if(getLength(e.x, e.y, plane.x, plane.y) < 35) {
-					plane.x = e.x - 16;
-					plane.y = e.y - 16;
-				}
-			});
+				function(e) {
+					if(getLength(e.x, e.y, plane.x, plane.y) < 35) {
+						plane.x = e.x - 16;
+						plane.y = e.y - 16;
+					}
+				});
 		game.rootScene.addEventListener('touchmove', 
-			function(e) {
-				if(getLength(e.x, e.y, plane.x, plane.y) < 35) { 
-					plane.x = e.x - 16;
-					plane.y = e.y - 16;
-				}
-			});
+				function(e) {
+					if(getLength(e.x, e.y, plane.x, plane.y) < 35) { 
+						plane.x = e.x - 16;
+						plane.y = e.y - 16;
+					}
+				});
 
 		game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
-			// create ring
-			game.tick++;
-			if( game.tick % 32 === 0 && createRings < 30) {
-				createRings++;
-        	                game.addSurface(rand(220), rand(220), "red");
-				if( createRings % 5 == 0) {
-					game.addSurface(rand(200), rand(200), "blue");
-				} else if( createRings % 12 == 0) {
-					game.addSurface(rand(200), rand(200), "green");
+				// create ring
+				game.tick++;
+				if( game.tick % 32 === 0 && createRings < 30) {
+					createRings++;
+					game.addSurface(rand(220), rand(220), "red");
+					if( createRings % 5 == 0) {
+						game.addSurface(rand(200), rand(200), "blue");
+					} else if( createRings % 12 == 0) {
+						game.addSurface(rand(200), rand(200), "green");
+					}
 				}
-			}
 		}); 
 	}
 
